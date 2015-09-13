5d7cca790a56a87c90c610f7102a0ff940fd2af6
Simon J Mudd <sjmudd@pobox.com>
2015-09-13 09:35:30 +0200

    Change prefix to group as clearer

--- anonymiser.go
+++ anonymiser.go
@@ -4,30 +4,32 @@ import (
 	"fmt"
 )
 
-// looks like a key cache but when we get a new name we return the prefix plus a number.
-// each new name has the number increased.
-// Can be used to convert private names into a public anonymous set (e.g. ps-top)
-// e.g.
-// Anonymise( "table", "tablea" )  --> table1
-// Anonymise( "table", "tableb" )  --> table2
-// Anonymise( "table", "tablea" )  --> table1
-// Anonymise( "db",    "my_db" )   --> db1
-// Anonymise( "db",    "otherdb" ) --> db2
-// Anonymise( "db",    "otherdb" ) --> db2
-// Anonymise( "db",    "my_db" )   --> db1
+/*
+Designed to anonymise a group of strings.
+We provide a group name and string and get back an anonymised string based on the group name.
+Can be used to convert private names into a public anonymous set (e.g. ps-top)
+e.g.
+Anonymise( "table", "tablea" )  --> table1
+Anonymise( "table", "tableb" )  --> table2
+Anonymise( "table", "tablea" )  --> table1
+Anonymise( "db",    "my_db" )   --> db1
+Anonymise( "db",    "otherdb" ) --> db2
+Anonymise( "db",    "otherdb" ) --> db2
+Anonymise( "db",    "my_db" )   --> db1
+*/
 
 type anonymous struct {
-	prefix string
-	last   int
-	id     map[string]int
+	group string
+	last  int
+	id    map[string]int
 }
 
 var (
-	prefixMap map[string]anonymous
-	enabled bool
+	groupMap map[string]anonymous
+	enabled  bool
 )
 
-// initinitialise structures
+// initialise structures - a global per app set of string groups
 func init() {
 	enabled = true
 	Clear()
@@ -37,12 +39,12 @@ func init() {
 func Clear() {
 	// no need to clean up explicitly if we had old data?
 	// I guess go garbage collects but might be nice to do this???
-	prefixMap = make(map[string]anonymous)
+	groupMap = make(map[string]anonymous)
 }
 
 // Enable the anonymiser. We provide a boolean to signal intent
-func Enable(set_enabled bool) {
-	enabled = set_enabled
+func Enable(setEnabled bool) {
+	enabled = setEnabled
 }
 
 // Enabled returns if anonymising is enabled
@@ -50,16 +52,16 @@ func Enabled() bool {
 	return enabled
 }
 
-// does the name exist already
-func (a anonymous) exists(name string) bool {
-	_, ok := a.id[name]
+// does the group name exist already?
+func (a anonymous) exists(group string) bool {
+	_, ok := a.id[group]
 	return ok
 }
 
 // return the anonymised name
 func (a *anonymous) name(orig string) string {
 	if a.exists(orig) {
-		return fmt.Sprintf("%s%d", a.prefix, a.id[orig])
+		return fmt.Sprintf("%s%d", a.group, a.id[orig])
 	}
 	return a.add(orig)
 }
@@ -71,20 +73,20 @@ func (a *anonymous) add(orig string) string {
 	return a.name(orig)
 }
 
-// Anonymise takes a prefix and name and returns a string consisting
-// of the prefix plus a number. If the string has been seen before
-// then the same name is returned.  Use different prefixes if you want
+// Anonymise takes a group and name and returns a string consisting
+// of the group plus a number. If the string has been seen before
+// then the same name is returned.  Use different groups if you want
 // to store different sets of names.
-func Anonymise(prefix, name string) string {
-	if ! enabled {
+func Anonymise(group, name string) string {
+	if !enabled {
 		return name
 	}
-	if _, ok := prefixMap[prefix]; !ok {
-		b := anonymous{prefix: prefix, id: make(map[string]int)}
+	if _, ok := groupMap[group]; !ok {
+		b := anonymous{group: group, id: make(map[string]int)}
 		b.add(name)
-		prefixMap[prefix] = b
+		groupMap[group] = b
 	}
 
-	a := prefixMap[prefix]
+	a := groupMap[group]
 	return a.name(name)
 }
